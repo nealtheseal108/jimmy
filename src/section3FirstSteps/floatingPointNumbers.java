8fb2d560ae8eab6330eafae4d3155618c677fd1d
nealtheseal108 <nealtheseal108@gmail.com>
2022-06-20 14:37:38 -0700

    bongesh

--- src/section3FirstSteps/floatingPointNumbers.java
+++ src/section3FirstSteps/floatingPointNumbers.java
@@ -12,8 +12,14 @@ public class floatingPointNumbers {
         System.out.println("Double Min Value = " + myMinDoubleValue);
         System.out.println("Double Max Value = " + myMaxDoubleValue);
 
-        int myIntValue = 5;
-        float myFloatValue = 5.25f;
-        double myDoubleValue = 5.25;
+        int myIntValue = 5 / 3;
+        float myFloatValue = 5 / 3f;
+        double myDoubleValue = 5.00 / 3.00;
+
+        System.out.println("int value = " + myIntValue);
+        System.out.println("float value = " + myFloatValue);
+        System.out.println("double value = " + myDoubleValue);
+
+
     }
 }
