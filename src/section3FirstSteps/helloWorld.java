5260c6a73d72daa2b4850cb9777ecc59cbefff71
nealtheseal108 <nealtheseal108@gmail.com>
2022-06-20 13:00:28 -0700

    ywa

---
+++ src/section3FirstSteps/helloWorld.java
@@ -0,0 +1,9 @@
+package section3FirstSteps;
+
+public class helloWorld {
+    public static void main(String[]args) {
+        System.out.println("Hello, Tim!");
+        int myFirstNumber = 1 + 2;
+        System.out.println(myFirstNumber);
+    }
+}
