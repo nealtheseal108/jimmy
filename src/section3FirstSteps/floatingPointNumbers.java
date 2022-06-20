8d92ed5ec9f9476150051cef0bbf71cdd4778869
nealtheseal108 <nealtheseal108@gmail.com>
2022-06-20 13:54:37 -0700

    bongesh

---
+++ src/section3FirstSteps/floatingPointNumbers.java
@@ -0,0 +1,15 @@
+package section3FirstSteps;
+
+public class floatingPointNumbers {
+    public static void main(String[] args) {
+        float myMinFloatValue = Float.MIN_VALUE;
+        float myMaxFloatValue = Float.MAX_VALUE;
+        System.out.println("Float Min Value = " + myMinFloatValue);
+        System.out.println("Float Max Value = " + myMaxFloatValue);
+
+        double myMinDoubleValue = Double.MIN_VALUE;
+        double myMaxDoubleValue = Double.MAX_VALUE;
+        System.out.println("Double Min Value = " + myMinDoubleValue);
+        System.out.println("Double Max Value = " + myMaxDoubleValue);
+    }
+}
