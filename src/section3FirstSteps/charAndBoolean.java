424cc1bf198464c6040ff047da9dec93969877bc
nealtheseal108 <nealtheseal108@gmail.com>
2022-06-21 08:05:07 -0700

    bongesh

--- src/section3FirstSteps/charAndBoolean.java
+++ src/section3FirstSteps/charAndBoolean.java
@@ -1,4 +1,16 @@
 package section3FirstSteps;
 
 public class charAndBoolean {
+    public static void main(String[] args) {
+
+        char myChar = 'd';
+
+        char unicodeChar = '\u00A9';
+        System.out.println(unicodeChar);
+
+        boolean myTrue = true;
+        boolean myFalse = false;
+
+        boolean isCustomerOver21 = true;
+    }
 }
