0f9bbb06cf563815293a5c6b59df15fb3070e3a3
nealtheseal108 <nealtheseal108@gmail.com>
2022-06-21 16:49:23 -0700

    bongesh

--- src/section4ExpressionsStatementsMethods/methods.java
+++ src/section4ExpressionsStatementsMethods/methods.java
@@ -7,6 +7,19 @@ public class methods {
 
         yourScore = calculateScore(true, 10000, 8, 200);
         System.out.println("Your final score was " + yourScore);
+
+        int highScorePosition;
+        highScorePosition = calculateHighScorePosition(1500);
+        displayHighScorePosition("Bob", highScorePosition);
+
+        highScorePosition = calculateHighScorePosition(900);
+        displayHighScorePosition("Joe", highScorePosition);
+
+        highScorePosition = calculateHighScorePosition(400);
+        displayHighScorePosition("Tim", highScorePosition);
+
+        highScorePosition = calculateHighScorePosition(50);
+        displayHighScorePosition("Harry", highScorePosition);
     }
 
     public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
@@ -19,6 +32,20 @@ public class methods {
     }
 
     public static void displayHighScorePosition(String playerName, int highScorePosition) {
-        System.out.println("Player " + playerName + " managed to get into position " + highScorePosition + " on the leaderboard.");
+        System.out.println(playerName + " managed to get into position " + highScorePosition + " on the leaderboard.");
+    }
+
+    public static int calculateHighScorePosition(int score) {
+        int highScorePosition = 4;
+        if (score > 1000) {
+            highScorePosition = 1;
+        } else if (score > 500) {
+            highScorePosition = 2;
+        } else if (score > 100) {
+            highScorePosition = 3;
+        }
+        return highScorePosition;
     }
 }
+
+// a conditional has to be false for it to reach the next conditional
