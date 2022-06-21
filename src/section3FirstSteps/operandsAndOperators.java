4119cb4ae371a83336e7e4333d48eff19adfba80
nealtheseal108 <nealtheseal108@gmail.com>
2022-06-21 08:23:34 -0700

    bongesh

---
+++ src/section3FirstSteps/operandsAndOperators.java
@@ -0,0 +1,10 @@
+package section3FirstSteps;
+
+public class operandsAndOperators {
+    public static void main(String[] args) {
+        // operands are any terms affected by operators
+        // an expression is formed by combining variable, literals, operators and method return values
+
+
+    }
+}
