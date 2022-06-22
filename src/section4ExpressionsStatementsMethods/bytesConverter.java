fb676d227b0ebf2d3e43d767ec4792f1a33568ce
nealtheseal108 <nealtheseal108@gmail.com>
2022-06-22 08:49:58 -0700

    bongesh

---
+++ src/section4ExpressionsStatementsMethods/bytesConverter.java
@@ -0,0 +1,11 @@
+package section4ExpressionsStatementsMethods;
+
+public class bytesConverter {
+    public static void printMegaBytesAndKiloBytes(int kiloBytes) {
+        if (kiloBytes < 0) {
+            System.out.println("Invalid Value");
+        } else {
+            System.out.println(kiloBytes + " KB = " + kiloBytes / 1024 + " MB and " + kiloBytes % 1024 + " KB");
+        }
+    }
+}
---
+++ src/section4ExpressionsStatementsMethods/leapYearCalculator.java
@@ -0,0 +1,4 @@
+package section4ExpressionsStatementsMethods;
+
+public class leapYearCalculator {
+}
