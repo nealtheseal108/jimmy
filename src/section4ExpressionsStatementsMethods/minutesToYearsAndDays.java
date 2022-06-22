7a969989253dce82da844ec93621aa6b2946ef86
nealtheseal108 <nealtheseal108@gmail.com>
2022-06-22 11:08:27 -0700

    bongesh

---
+++ src/section4ExpressionsStatementsMethods/minutesToYearsAndDays.java
@@ -0,0 +1,14 @@
+package section4ExpressionsStatementsMethods;
+
+public class minutesToYearsAndDays {
+    public static void printYearsAndDays (long minutes) {
+        if (minutes < 0) {
+            System.out.println("Invalid Value");
+            return;
+        }
+        long years = minutes / 525600;
+        long remainingMinutes = minutes % 525600;
+        long days = remainingMinutes / 1440;
+        System.out.println(minutes + " min = " + years + " y and " + days + " d");
+    }
+}
