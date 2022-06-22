c933489a54ac8a50553aa361c0947b81cbd70e83
nealtheseal108 <nealtheseal108@gmail.com>
2022-06-22 11:22:58 -0700

    bongesh

---
+++ src/section4ExpressionsStatementsMethods/playingCat.java
@@ -0,0 +1,16 @@
+package section4ExpressionsStatementsMethods;
+
+public class playingCat {
+    public static boolean isCatPlaying(boolean summer, int temperature) {
+        if (!summer) {
+            if (temperature >= 25 && temperature <= 35) {
+                return true;
+            }
+            return false;
+        }
+        if (temperature >= 25 && temperature <= 45) {
+            return true;
+        }
+        return false;
+    }
+}
---
+++ src/section5ControlFlowStatements/switchStatements.java
@@ -0,0 +1,7 @@
+package section5ControlFlowStatements;
+
+public class switchStatements {
+    public static void main(String[] args) {
+
+    }
+}
