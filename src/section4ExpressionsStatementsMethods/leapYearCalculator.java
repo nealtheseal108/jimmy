752fae024be1318d1672b0d7c40d647e4741f97b
nealtheseal108 <nealtheseal108@gmail.com>
2022-06-22 09:10:53 -0700

    bongesh

--- src/section4ExpressionsStatementsMethods/leapYearCalculator.java
+++ src/section4ExpressionsStatementsMethods/leapYearCalculator.java
@@ -1,4 +1,17 @@
 package section4ExpressionsStatementsMethods;
 
 public class leapYearCalculator {
+    public static boolean isLeapYear (int year) {
+        if (year < 1 || year > 9999) {
+            return false;
+        } else if (year % 4 == 0) {
+            if (year % 400 == 0) {
+                return true;
+            }
+            if (year % 100 != 0) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
