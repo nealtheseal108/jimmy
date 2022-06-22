70f1df86381eeb08748ed55f3e822a2e035ed5d9
nealtheseal108 <nealtheseal108@gmail.com>
2022-06-22 09:12:52 -0700

    bongesh

---
+++ src/section4ExpressionsStatementsMethods/equalSumChecker.java
@@ -0,0 +1,10 @@
+package section4ExpressionsStatementsMethods;
+
+public class equalSumChecker {
+    public static boolean hasEqualSum (int x, int y, int z) {
+        if (x + y == z) {
+            return true;
+        }
+        return false;
+    }
+}
