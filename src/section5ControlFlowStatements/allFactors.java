ba95333ca874213705ce6aaa4b649b288866b9f9
nealtheseal108 <nealtheseal108@gmail.com>
2022-06-23 09:30:48 -0700

    bongesh

---
+++ src/section5ControlFlowStatements/allFactors.java
@@ -0,0 +1,20 @@
+package section5ControlFlowStatements;
+
+public class allFactors {
+    public static void printFactors (int number) {
+
+        if (number < 1) {
+            System.out.println("Invalid Value");
+            return;
+        }
+
+        String factors = "";
+        for (int i = 1; i <= number; i++) {
+            if (number % i == 0) {
+                factors += Integer.toString(i) + " ";
+            }
+        }
+
+        System.out.println(factors);
+    }
+}
