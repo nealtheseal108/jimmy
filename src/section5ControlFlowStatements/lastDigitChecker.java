932b68b67e816a7cb93199306083eecb748e8a9c
nealtheseal108 <nealtheseal108@gmail.com>
2022-06-23 09:19:39 -0700

    bongesh

---
+++ src/section5ControlFlowStatements/lastDigitChecker.java
@@ -0,0 +1,27 @@
+package section5ControlFlowStatements;
+
+public class lastDigitChecker {
+
+    public static boolean hasSameLastDigit (int x, int y, int z) {
+        if (x < 10 || x > 1000 || y < 10 || y > 1000 || z < 10 || z > 1000) {
+            return false;
+        }
+
+        int lastDigitX = (x % 10);
+        int lastDigitY = (y % 10);
+        int lastDigitZ = (z % 10);
+
+        if (lastDigitX == lastDigitY || lastDigitX == lastDigitZ || lastDigitY == lastDigitZ) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public static boolean isValid (int x) {
+        if (x < 10 || x > 1000) {
+            return false;
+        }
+        return true;
+    }
+}
