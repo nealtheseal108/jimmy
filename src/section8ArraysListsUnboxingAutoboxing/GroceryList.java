9e2ee53d4b4d287c27e573fcc384b798a079ad55
nealtheseal108 <nealtheseal108@gmail.com>
2022-06-28 15:43:39 -0700

    bongesh

--- src/section8ArraysListsUnboxingAutoboxing/GroceryList.java
+++ src/section8ArraysListsUnboxingAutoboxing/GroceryList.java
@@ -13,33 +13,38 @@ public class GroceryList {
     public void printGroceryList() {
         System.out.println("You have " + groceryList.size() + " items in your grocery list.");
         for (int i = 0; i < groceryList.size(); i++) {
-            System.out.println("Item #" + i + 1 + " is " + groceryList.get(i) + ".");
+            System.out.println("Item #" + (int) (i + 1) + " is " + groceryList.get(i) + ".");
         }
     }
 
     public void modifyGroceryList(int position, String newItem) {
-        if (position < 1) {
-            System.out.println("You cannot modify a negative or zero position in your list.");
-        }
-        groceryList.set(position - 1, newItem);
-        System.out.println("Item #" + (position - 1) + " was replaced with " + groceryList.get(position - 1) + ".");
+        groceryList.set(position, newItem);
+        System.out.println("Item #" + (position + 1) + " was replaced with " + groceryList.get(position) + ".");
     }
 
-    public void removeGroceryItem(int position) {
+    public void removeGroceryItem(String item) {
+        int position = findItem(item);
+        if (position != -1) {
+            groceryList.remove(item);
+        }
+    }
+
+    private void removeGroceryItem(int position) {
         if (position < 1) {
             System.out.println("You cannot remove a negative or zero position in your list.");
         }
         String item = groceryList.get(position - 1);
         groceryList.remove(position - 1);
+        System.out.println(groceryList.get(position - 1) + " was removed.");
     }
 
-    public String findItem(String searchItem) {
+    public int findItem(String searchItem) {
         // boolean exists = groceryList.contains(searchItem);
-        int position = groceryList.indexOf(searchItem);
-        if (position >= 0) {
-            groceryList.get(position);
-        }
-        return null;
+        return groceryList.indexOf(searchItem);
+    }
+
+    public ArrayList<String> getGroceryList() {
+        return this.groceryList;
     }
 }
 // the collection in Java is a framework that provides architecture to store and manipulate objects
@@ -54,3 +59,4 @@ public class GroceryList {
 // use '.remove' to remove a certain index in an ArrayList
 // use '.contains' to view if an item exists in an ArrayList (returns boolean)
 // use '.indexOf' to find the first occurrence of an object in an ArrayList (returns int; returns -1 is nothing is found)
+// use '.addAll' to add the entire contents of one ArrayList to another
