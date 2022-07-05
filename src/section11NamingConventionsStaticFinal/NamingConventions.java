d8ac152935db1b0a5b2f68f0162b164f1c5250ca
nealtheseal108 <nealtheseal108@gmail.com>
2022-07-05 09:39:30 -0700

    bongesh

---
+++ src/section11NamingConventionsStaticFinal/NamingConventions.java
@@ -0,0 +1,15 @@
+package section11NamingConventionsStaticFinal;
+
+public class NamingConventions {
+}
+
+// adopting naming conventions in Java makes it easier for others and yourself to understand
+// packages: always lowercase, unique, use your internet domain name reversed ('com.nealtheseal108')
+// replace invalid characters (ex: '-') in the domain name with an underscore; domain name components starting with a number should instead start with an underscore
+// domain name components that are Java keywords should have that component start with an underscore
+// class and interface names should use PascalCase, should be intuitive
+// method names should be camelCase, verbs that reflect their function
+// constants should be all uppercase, words should be separated by underscores
+// field names should be camelCase, meaningful and indicative, should not utilize underscores
+// type parameters should be single capital letters, E - element, T - type, K - key, V - value
+
