dd131a1d9d0c6c311f6bde0d6333163cbb35f657
nealtheseal108 <nealtheseal108@gmail.com>
2022-07-05 13:09:26 -0700

    bongesh

---
+++ src/section11NamingConventionsStaticFinal/ScopeMain.java
@@ -0,0 +1,22 @@
+package section11NamingConventionsStaticFinal;
+
+public class ScopeMain {
+    public static void main(String[] args) {
+        String var4 = "this is private to main()";
+
+        ScopeCheck scopeInstance = new ScopeCheck();
+        System.out.println("scopeInstance var1 is " + scopeInstance.getVar1());
+        System.out.println(var4);
+
+        scopeInstance.timesTwo();
+
+        System.out.println("**********************************************");
+
+        ScopeCheck.InnerClass innerClass = scopeInstance.new InnerClass();
+        innerClass.timesTwo();
+
+    }
+}
+
+// in this case, there are two variables in different public classes called privateVar, but the access modifiers of both (private) allow them to coexist (and because they are in different classes)
+
