9f95d271f08f49cb8f3dbe820e086df55d05354c
nealtheseal108 <nealtheseal108@gmail.com>
2022-07-05 13:42:29 -0700

    bongesh

---
+++ src/section11NamingConventionsStaticFinal/X.java
@@ -0,0 +1,17 @@
+package section11NamingConventionsStaticFinal;
+
+import java.util.Scanner;
+
+public class X {
+    private int x;
+
+    public X(int x) {
+        this.x = x;
+    }
+
+    public void x() {
+        for (int x = 1; x <= 12; x++) {
+            System.out.println(this.x + " times " + x + " = " + this.x * x);
+        }
+    }
+}
