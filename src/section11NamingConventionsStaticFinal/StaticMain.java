5dd2fcf0f91b7bf75e697aea901e94ce15c2c3b9
nealtheseal108 <nealtheseal108@gmail.com>
2022-07-06 10:51:44 -0700

    bongesh

--- src/section11NamingConventionsStaticFinal/StaticMain.java
+++ src/section11NamingConventionsStaticFinal/StaticMain.java
@@ -5,17 +5,38 @@ public class StaticMain {
     public static int multiplier = 7;
 
     public static void main(String[] args) {
-//        StaticTest firstInstance = new StaticTest("1st Instance");
-//        System.out.println(firstInstance.getName() + " is instance #" + StaticTest.getNumInstances());
-//
-//        StaticTest secondInstance = new StaticTest("2nd Instance");
-//        System.out.println(secondInstance.getName() + " is instance #" + StaticTest.getNumInstances());
-//
-//        StaticTest thirdInstance = new StaticTest("3rd Instance");
-//        System.out.println(thirdInstance.getName() + " is instance #" + StaticTest.getNumInstances());
+        StaticTest firstInstance = new StaticTest("1st Instance");
+        System.out.println(firstInstance.getName() + " is instance #" + StaticTest.getNumInstances());
+
+        StaticTest secondInstance = new StaticTest("2nd Instance");
+        System.out.println(secondInstance.getName() + " is instance #" + StaticTest.getNumInstances());
+
+        StaticTest thirdInstance = new StaticTest("3rd Instance");
+        System.out.println(thirdInstance.getName() + " is instance #" + StaticTest.getNumInstances());
         int answer = multiply(6);
         System.out.println("The answer is " + answer + ".");
         System.out.println("Multiplier = " + multiplier);
+
+
+        FinalTest one = new FinalTest("one");
+        FinalTest two = new FinalTest("two");
+        FinalTest three = new FinalTest("three");
+
+        System.out.println(one.getInstanceNumber());
+        System.out.println(two.getInstanceNumber());
+        System.out.println(three.getInstanceNumber());
+
+        System.out.println(Math.PI);
+        // Math m = new Math();
+
+        Password password = new Password(123456);
+        password.storePassword();
+        password.letMeIn(12345);
+
+        System.out.println("Main method called.");
+        SIBTest test = new SIBTest();
+        test.someMethod();
+        System.out.println("Owner is " + SIBTest.owner);
     }
 
     public static int multiply(int number) {
@@ -32,3 +53,7 @@ public class StaticMain {
 // the 'non-static field/method _____ cannot be referenced from a static context' displays as a result of non-static fields/methods being required to be instantiated using an instance of the clas they were to be accessed
 // non-static fields/methods don't exist until an instance of the class they are within is created
 // you can use static fields/methods from a non-static context w/ no problems even if they are in another class
+// a static initialisation block is the opposite of a constructor
+// whereas constructors are outlined at every instantiation of a class, static initializers are executed only once when the class is first loaded
+// all static final fields must be initialized before all static initialisation blocks terminate
+//
